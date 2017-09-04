1120bcc8e29529b2c29ec9857f4579b4e48a6a93
syxtest01@gmail.com <xing_*666888>
2017-09-04 13:55:17 +0800

    新建项目，并测试orleans

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Orleans;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,28 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            //等待服务端启动完毕
+            Console.ReadKey();
+            //然后我聪明的敲击了回车键
+            Run();
+            Console.ReadLine();
+        }
+
+        static async void Run()
+        {
+            //利用内置方法获得一个配置类,这个类指明服务端的端口是1234
+            //可以利用配置文件,不过这里我就先用这个简单的配置类
+            var config = Orleans.Runtime.Configuration.ClientConfiguration.LocalhostSilo(1234);
+
+            // 初始化一个GrainClient
+            GrainClient.Initialize(config);
+
+            // 从silo处，获得一个BasicGrain的接口
+            IGrains.IBasic agrain = GrainClient.GrainFactory.GetGrain<IGrains.IBasic>(314);
+
+            //调用里面的方法，等待它返回
+            string result = await agrain.SayHello("可以");
+            Console.WriteLine(result);
         }
     }
 }
---
+++ Host/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //获得一个配置实例
+            //它需要两个端口,第一个端口2234是用来silo与silo之间的通信的,第二个1234是用于监听client的请求的
+            var config = Orleans.Runtime.Configuration.ClusterConfiguration.LocalhostPrimarySilo(2234, 1234);
+
+            //初始化一个silohost,这里使用了Orleans提供的silohost而不是silo,其中silo的名字命名为Ba;
+            Orleans.Runtime.Host.SiloHost silohost = new Orleans.Runtime.Host.SiloHost("Ba", config);
+            silohost.InitializeOrleansSilo();
+            silohost.StartOrleansSilo();
+
+            //检查一下
+            if (silohost.IsStarted)
+            {
+                Console.WriteLine("siloHost 启动成功");
+            }else
+            {
+                Console.WriteLine("启动失败");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
